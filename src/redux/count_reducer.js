af84a63c056dbdbe98c26dd8a1dc1d8606c3d7bd
Gloria <hanning_hit@hotmail.com>
2022-06-01 10:41:50 +1000

    test1

--- src/redux/count_reducer.js
+++ src/redux/count_reducer.js
@@ -1,7 +1,7 @@
 
 const initState = 0
 export default function countReducer(preState=initState,action){
-    // 解构赋值，从action对象中获取元素type和data
+    // 解构赋值，从action对象中获取元素type和datak
     const {type,data} = action
     switch (type) {
         case 'increment':
