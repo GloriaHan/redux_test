d84f8e60c188d47d6be6bcdd592ec931d5a006e3
Gloria <hanning_hit@hotmail.com>
2022-06-01 10:37:05 +1000

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+// 引入creatStore，专门用于创建redux中最为核心的store对象
+import  {createStore } from 'redux'
+// 引入为Count组件服务的reducer
+import countReducer from './count_reducer'
+export default createStore(countReducer)
